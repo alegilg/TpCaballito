1862282f98f95f89e17ded2ad5d503275eec08b9
NumaLeone <numaleoneelizalde@gmail.com>
2019-03-20 17:30:29 -0300

    asd

--- src/TpCaballito/Board.java
+++ src/TpCaballito/Board.java
@@ -3,7 +3,13 @@ package TpCaballito;
 public class Board {
     Board[][] board;
 
+    int initialPositionX = 1;
+    int initialPositionY = 1;
+
+    Horse horse =  new Horse(initialPositionX,initialPositionY);
     public Board(Board[][] board) {
         this.board = board;
     }
+
+
 }
