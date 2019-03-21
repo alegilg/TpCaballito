8fc4162944ea1011d02b33ef7ff3458123e69237
Ale <alejandro.gilgargantini@ing.austral.edu.ar>
2019-03-21 16:37:26 -0300

    hui

---
+++ src/TpCaballito/Test.java
@@ -0,0 +1,11 @@
+package TpCaballito;
+
+public class Test
+{
+    public static void main(String[] args) {
+        Board[][] board  =  new Board[8][8];
+
+        System.out.println(position);
+
+    }
+}
